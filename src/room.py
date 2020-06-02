bc79eaffd7ccb218cb2b3029ea37b46ebfbd51d2
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-06-02 14:12:56 -0700

    Make Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,23 @@
+from __future__ import annotations
+
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+__all__ = ('Room',)
+
+from typing import Dict, Optional
+
+
+class Room:
+    n_to: Optional[Room] = None
+    e_to: Optional[Room] = None
+    s_to: Optional[Room] = None
+    w_to: Optional[Room] = None
+
+    def __init__(self, name: str, surroundings: str) -> None:
+        self.name = name
+        self.surroundings = surroundings
+
+    @property
+    def directions(self) -> Dict['str', Room]:
+        return {'n': self.n_to, 'e': self.e_to, 's': self.s_to, 'w': self.w_to}
