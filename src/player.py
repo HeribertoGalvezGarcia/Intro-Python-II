fd3412a8d646a107401a3b20b71f63cf58097d26
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-06-02 14:13:16 -0700

    Make Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+__all__ = ('Player',)
+
+from typing import Optional
+
+from .room import Room
+
+
+class Player:
+    current_room: Optional[Room] = None
